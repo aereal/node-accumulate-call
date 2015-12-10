641f1ff161c93df589aff8c53378accc980025f9
aereal <aereal@aereal.org>
2015-12-10 19:40:51 +0900

    Add initial implementation

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export default function bufferedApply<T>(f: (buffer: T[]) => void, wait: number): (t: T) => void {
+  let buffer: T[] = [];
+  return (t: T): void => {
+    buffer.push(t);
+    setInterval(() => {
+      if (buffer.length > 0) {
+        f(buffer);
+        buffer = [];
+      }
+    }, wait);
+  };
+}
