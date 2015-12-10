0a8ded07762e61248139cea6b8c47cf490da8532
aereal <aereal@aereal.org>
2015-12-11 00:01:12 +0900

    + docs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+/**
+ * `accumulateUntil` creates a new function calls given function `f` that takes a array of buffered arguments and called at least every `untilGap` milliseconds.
+ */
 export function accumulateUntil<T>(f: (buffer: T[]) => void, untilGap: number): (t: T) => void {
   let buffer: T[] = [];
   return (t: T): void => {
